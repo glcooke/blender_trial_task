ac38d1e4a72d62a26e308dad9ac4c1122faee8f6
cooke <iochernyshev@outlook.cin>
2023-09-25 19:42:23 +0200

    Cleanup

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,5 +1,7 @@
 pipeline {
-    agent any
+    agent {
+        label 'agent-name'
+    }
     stages {
         stage('Dir cleaning') {
             steps {
@@ -11,7 +13,6 @@ pipeline {
         stage('Checkout from GitHub') {
             steps {
                 script {
-                    // Checkout the GitHub repository
                     checkout([$class: 'GitSCM', branches: [[name: 'main']], userRemoteConfigs: [[url: 'https://github.com/glcooke/blender_trial_task']]])
                 }
             }
@@ -19,14 +20,12 @@ pipeline {
         stage('Run Scripts') {
             steps {
                 script {
-                    // Access the parameter value
                     def blender_path = params.blender_path
                     def output_path = params.output_path
                     def x_resolution = params.x_resolution
                     def y_resolution = params.y_resolution
                 }
                 withPythonEnv('D:\\jenkins\\Blender Node\\workspace\\Python310\\python.exe') {
-                    // Execute your scripts here
                     bat '''
                         pip install -r requirements.txt
                         python main.py %blender_path% %output_path% %x_resolution% %y_resolution%
